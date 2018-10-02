797b4d7f96dbb8b5f823c8877a4fd25e6160c551
steven real sanchez <stevenn9549@gmail.com>
2018-10-02 15:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function logWhisper(string){
   return console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string)
-{ if 
+function sayHiToGrandma(string){
+  if (string===string.toLowerCase())
+}
