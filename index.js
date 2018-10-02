645154354ed135a755b24c91bd8ba0af83d71720
steven real sanchez <stevenn9549@gmail.com>
2018-10-02 15:22:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+return string.toUpperCase()
+  }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logshout(string){
+  return console.log(string.toUpperCase())
+}
+
+function logwhisper(string){
+  return console.log(string.toLowerCase())
+}
+
