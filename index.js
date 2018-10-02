c4d16f45611753f25fe9de25491ac6d7a13b1721
steven real sanchez <stevenn9549@gmail.com>
2018-10-02 15:50:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ function logWhisper(string){
   return console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   if (string===string.toLowerCase()) return"i can't hear you;
-  else if(string===string.toUpperCase()) return "YES INDEED!"
-  else if(string==="I love you, Grandma.") return "I love you, too"
-  
-}
+  else if(string===string.toUpperCase()) return "YES INDEED!";
+  else if(string==="I love you, Grandma.") return "I love you, too";
+  }
